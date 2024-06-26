7791fe28f9c9234de2aea4e0fb77533a919828d0
matsumo-and <yh134.toisanda@gmail.com>
2024-06-27 02:58:36 +0900

    first commit

---
+++ src/main/java/com/spring/java/batch/work_spring_batch/WorkSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.spring.java.batch.work_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/java/batch/work_spring_batch/WorkSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.java.batch.work_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
